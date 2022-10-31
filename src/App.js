801d99d114a29e9787aa4c84e5395f641722186b
JaeiKim <dobbykim0320@gmail.com>
2022-10-31 16:59:46 +0900

    modified

--- src/App.js
+++ src/App.js
@@ -45,21 +45,28 @@ const QuestionImag = (props) => {
 const Answer = (props) => {
   const navigation = useNavigate();
 
-  return (
-    <div>
-      <button
-        className="btn"
-        onClick={() => {
-          const pathname = window.location.pathname;
-          const nextPathname =
-            parseInt(pathname.charAt(pathname.length - 1)) + 1;
+  const { setDispatchType } = React.useContext(StoreContext);
 
-          navigation(`/p${nextPathname}`);
-        }}
-      >
-        {props.text}
-      </button>
-    </div>
+  return (
+    <button
+      className="btn"
+      onClick={() => {
+        setDispatchType({
+          code: "답변",
+          params: {
+            value: props.value,
+          },
+        });
+
+        // const pathname = window.location.pathname;
+        // const nextStep = parseInt(pathname.charAt(pathname.length - 1)) + 1;
+        // navigation(`/on${nextStep}`);
+
+        // navigation('');
+      }}
+    >
+      {props.text}
+    </button>
   );
 };
 
@@ -69,9 +76,14 @@ function Page1() {
       <div className="body">
         <ProgressBar step={1} />
         <QuestionImag image="	https://kakaofriendsmbti.netlify.app/images/01-01.png" />
-        <Answer text="당연하지! 어디서 할지 고민 중이야!" nextPathname={1} />
+        <Answer
+          text="당연하지! 어디서 할지 고민 중이야!"
+          value="E"
+          nextPathname={1}
+        />
         <Answer
           text="그냥 맛있는거 먹으러 갈까 생각 중이야!"
+          value="I"
           nextPathname={1}
         />
       </div>
@@ -85,9 +97,14 @@ function Page2() {
       <div className="body">
         <ProgressBar step={2} />
         <QuestionImag image="	https://kakaofriendsmbti.netlify.app/images/02-01.png" />
-        <Answer text="영화 완전 재미었어! 너도 한번 봐봐!" nextPathname={2} />
+        <Answer
+          text="영화 완전 재미었어! 너도 한번 봐봐!"
+          value="S"
+          nextPathname={2}
+        />
         <Answer
           text="좀비가 너무 리얼했어. 실제 상황이면 난 바로 죽었을거야..."
+          value="N"
           nextPathname={2}
         />
       </div>
@@ -101,8 +118,8 @@ function Page3() {
       <div className="body">
         <ProgressBar step={3} />
         <QuestionImag image="		https://kakaofriendsmbti.netlify.app/images/03-01.png" />
-        <Answer text="무슨 꽃 샀어? 향은 좋아?" nextPathname={3} />
-        <Answer text="왜 우울해? 무슨 일 있어?" nextPathname={3} />
+        <Answer text="무슨 꽃 샀어? 향은 좋아?" value="T" nextPathname={3} />
+        <Answer text="왜 우울해? 무슨 일 있어?" value="F" nextPathname={3} />
       </div>
     </div>
   );
@@ -116,9 +133,14 @@ function Page4() {
         <QuestionImag image="	https://kakaofriendsmbti.netlify.app/images/04-01.png" />
         <Answer
           text="지금 PPT 만드는 중이니까 아마 한 2시간 뒤면 끝날거 같아!"
+          value="J"
           nextPathname={4}
         />
-        <Answer text="모르겠어. 근데 지금 PPT 만들고 있어!" nextPathname={4} />
+        <Answer
+          text="모르겠어. 근데 지금 PPT 만들고 있어!"
+          value="P"
+          nextPathname={4}
+        />
       </div>
     </div>
   );
@@ -131,10 +153,12 @@ function Page5() {
       <QuestionImag image="		https://kakaofriendsmbti.netlify.app/images/05-01.png" />
       <Answer
         text="그래! 역시 사람 많고 유명한 벚꽃 명소가 예쁘겠지 어디로 갈까?"
+        value="E"
         nextPathname={5}
       />
       <Answer
         text="그래! 사람 적은 벚꽃 명소 한 번 찾아볼까?"
+        value="I"
         nextPathname={5}
       />
     </div>
@@ -142,20 +166,71 @@ function Page5() {
 }
 
 function Result() {
-  return <div>Result</div>;
+  return <div>결과화면 !!</div>;
 }
+const StoreContext = React.createContext({});
 
+/**
+ *
+ * React useReducer [비지니스 로직 분리되어있는걸 한곳에 마법~]
+ */
 function App() {
+  const [dispatch, setDispatchType] = React.useState({
+    code: null,
+    params: null,
+  });
+  const [mbti, setMbti] = React.useState([
+    {
+      I: 0, // 내향
+      E: 0, // 외향
+    },
+    {
+      S: 0, // 현실
+      N: 0, // 이상주의
+    },
+    {
+      T: 0, // 이성
+      F: 0, // 감성
+    },
+    {
+      P: 0, // 즉흥
+      J: 0, // 계획
+    },
+  ]);
+
+  React.useEffect(() => {
+    switch (dispatch.code) {
+      /**
+       * mbti state값을 바꾸는 로직 구현
+       */
+      case "답변":
+        const { value } = dispatch.params;
+
+        break;
+
+      /**
+       * 로그인 하는 로직 구현
+       */
+      case "로그인":
+        break;
+
+      default:
+        break;
+    }
+  }, [dispatch]);
+
   return (
-    <Routes>
-      <Route exact path="/" element={<Main />}></Route>
-      <Route exact path="/p1" element={<Page1 />}></Route>
-      <Route exact path="/p2" element={<Page2 />}></Route>
-      <Route exact path="/p3" element={<Page3 />}></Route>
-      <Route exact path="/p4" element={<Page4 />}></Route>
-      <Route exact path="/p5" element={<Page5 />}></Route>
-      <Route exact path="/p6" element={<Result />}></Route>
-    </Routes>
+    <StoreContext.Provider value={{ setDispatchType }}>
+      <Routes>
+        <Route exact path="/" element={<Main />}></Route>
+        <Route exact path="/p1" element={<Page1 />}></Route>
+        <Route exact path="/p2" element={<Page2 />}></Route>
+        <Route exact path="/p3" element={<Page3 />}></Route>
+        <Route exact path="/p4" element={<Page4 />}></Route>
+        <Route exact path="/p5" element={<Page5 />}></Route>
+        <Route exact path="/p6" element={<Result />}></Route>
+      </Routes>
+    </StoreContext.Provider>
   );
 }
 
